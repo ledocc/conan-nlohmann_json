2cfdb96ba57fd373b0cdbc6bc9c2155edf69c401
David Callu <callu.david@gmail.com>
2019-07-18 16:55:23 +0200

    feat(ci): improve appveyor

--- build.py
+++ build.py
@@ -4,12 +4,16 @@ import platform
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder = ConanMultiPackager(
+        reference = "nlohmann_json/"+tools.load("version.txt")
+        channel = "testing"
+        stable_branch_pattern = "release/*"
+    )
+    builder.add_common_builds(pure_c=False)
 
     filtered_items = []
     for settings, options, env_vars, build_requires, reference in builder.items:
-        settings["compiler.cppstd"] = "14"
+        settings["compiler.cppstd"] = "11"
         if settings["compiler"] == "gcc":
             settings["compiler.libcxx"] = "libstdc++11"
         elif ( settings["compiler"] == "clang" ) and ( "compiler.libcxx" in settings ) and ( settings["compiler.libcxx"] == "libstdc++" ):
