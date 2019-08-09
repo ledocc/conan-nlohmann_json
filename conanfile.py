716ee2251543f9d0dce401990d14eb065045707f
David Callu <callu.david@gmail.com>
2019-08-09 07:46:10 +0200

    feat(conan): disable test on MSVC in debug mode

--- conanfile.py
+++ conanfile.py
@@ -26,15 +26,12 @@ class NlohmannjsonConan(ConanFile):
         tools.get( self.homepage+"/archive/"+archive_name,
                    sha256="80c45b090e40bf3d7a7f2a6e9f36206d3ff710acfa8d8cc1f8c763bb3075e22e")
 
+
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
-
-        if tools.cross_building(self.settings):
-            self.output.warn("Skipping run cross built package")
-            return
-
-        self.run("ctest --output_on_failure --timeout=3000", cwd=cmake.build_folder)
+        if self._should_build_test() and self._should_run_test():
+            self.run("ctest --output_on_failure --timeout=3000", cwd=cmake.build_folder)
 
     def package(self):
         cmake = self._configure_cmake()
@@ -46,6 +43,20 @@ class NlohmannjsonConan(ConanFile):
         cmake = CMake(self, set_cmake_flags=True)
         cmake.generator="Ninja"
         cmake.verbose=True
-        cmake.configure(source_dir="../json-"+self.version, build_dir="build")
+        if not self._should_build_test():
+            cmake.definitions["BUILD_TESTING"]="OFF"
 
+        cmake.configure(source_dir="../json-"+self.version, build_dir="build")
         return cmake
+
+    def _should_build_test(self):
+        if ( self.settings.get_safe("compiler") == "Visual Studio" ) and ( self.settings.get_safe("build_type") == "Debug" ) :
+            self.output.warn("Skipping test : Visual Studio build in Debug mode fail to compile.")
+            return False
+        return True
+
+    def _should_run_test(self):
+        if tools.cross_building(self.settings):
+            self.output.warn("Skipping test : cross built package.")
+            return False
+        return True
